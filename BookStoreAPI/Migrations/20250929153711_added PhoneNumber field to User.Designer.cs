152dca4ec8a92a98133eb3b814b2089ecf044bef
Petro <petrobokalo1@gmail.com>
2025-09-29 18:43:34 +0300

    added PhoneNumber field to User

--- BookStoreAPI/Migrations/20250929153711_added PhoneNumber field to User.Designer.cs
+++ BookStoreAPI/Migrations/20250929153711_added PhoneNumber field to User.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookStoreAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BookStoreAPI.Migrations
 {
     [DbContext(typeof(BookStoreDbContext))]
-    partial class BookStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250929153711_added PhoneNumber field to User")]
+    partial class addedPhoneNumberfieldtoUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
